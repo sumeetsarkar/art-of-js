e88c0729af4a6c005cc554a3d131e7e797ba94d7
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-10-24 18:56:58 +0530

    Updated function currying examples

--- func-curry/index.js
+++ func-curry/index.js
@@ -1,16 +1,35 @@
 (function(context) {
 
-  function multiply(a, b) { return a * b; }
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function greet(language, person) {
+    const greet = language === 'es' ? 'Hola' : 'Hello';
+    console.log(`${greet} ${person.fname} ${person.lname}`)
+  }
 
   function demo() {
-    console.log('\n\nFUNCTION CURRYING');
 
     const twice = multiply.bind(this, 2);
     const thrice = multiply.bind(this, 3);
-
-    console.log(twice(10));
+    
+    console.log(twice(5));
     console.log(thrice(10));
-  };
+
+    const johnSnow = {
+      fname: 'John',
+      lname: 'Snow'
+    };
+
+    greet('es', johnSnow);
+
+    const greetInSpanish = greet.bind(this, 'es');
+    const greetInEnglish = greet.bind(this, 'en');
+
+    greetInSpanish(johnSnow);
+    greetInEnglish(johnSnow);
+  }
 
   (context || this).demoLibs['func-curry'] = demo;
 })(window);
