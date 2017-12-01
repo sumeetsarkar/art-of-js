0a35d7b622cc98e8ffc5924ba5b6b592c0c7db01
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-12-01 18:54:19 +0530

    documented closure example

--- closures/index.js
+++ closures/index.js
@@ -1,5 +1,17 @@
 (function(context) {
 
+  // longRunningTask is a simple function taking url,
+  // & callbacks for success and failure
+  function longRunningTask(url, success, failure) {
+    if(url) {
+      return success('{\"name\":\"John Doe\",\"age\":27}');
+    } else {
+      return failure(404, 'no resource found');
+    }
+  }
+
+  // successCallback returns another function which can be called later,
+  // whilst still having access to data param in successCallback
   function successCallback(data) {
     return function() {
       const minimumAge = 21;
@@ -11,14 +23,6 @@
     }
   }
 
-  function longRunningTask(url, success, failure) {
-    if(url) {
-      return success('{\"name\":\"John Doe\",\"age\":27}');
-    } else {
-      return failure(404, 'no resource found');
-    }
-  }
-
   function failureCallback(error, reason) {
     return function() {
       console.log(`${error} - ${reason}`);
@@ -27,7 +31,9 @@
 
   function demo() {
     console.log('\n\nCLOSURES');
+    // returns function from successCallback
     const returnVal = longRunningTask('sample-url', successCallback, failureCallback);
+    // prints the data
     console.log(returnVal());
   };
 
