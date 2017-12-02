8d85ca58cfcbd0d6e2e6d70509fd91bb6c17ce2a
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-12-02 12:18:51 +0530

    updated promises example comments

--- promises/index.js
+++ promises/index.js
@@ -3,11 +3,15 @@
   function demo() {
     console.log('\n\nPROMISES');
 
+    // a promise generator function, returning new promise
+    // which executes a setTimeout resolving after 1 sec
     const timeoutPromise = () => new Promise((resolve, reject) => { setTimeout(resolve, 1000) });
     
+    // simple promise and then
     timeoutPromise()
       .then(() => console.log('Yo!'));
 
+    // promise chaining
     timeoutPromise()
       .then(() => timeoutPromise())
       .then(() => console.log('YoYo!'));
